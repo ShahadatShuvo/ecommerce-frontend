2a3675a8dfb794f0cbd774741c1e1d7cbfa9fbd3
ShahadatShuvo <hossainshuvo7860@gmail.com>
2022-09-01 14:09:00 +0600

    updated

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -17,28 +17,77 @@ const StyledBadge = styled(Badge)<BadgeProps>(({ theme }) => ({
 }));
 
 function Navbar({ cart }: any) {
+  const [pointernav, setPointerNav] = React.useState("/");
   return (
     <div className="w-full py-3 my-4 rounded-lg px-8 mx-auto bg-gray-100 flex flex-col md:flex-row flex-wrap justify-between text-gray-600">
       <div className="order-2 md:order-1 w-full md:w-1/3 flex justify-center md:justify-start mt-3 md:mt-0 items-center">
         <Link href="/">
-          <a className="mr-5 hover:text-gray-900">Home</a>
+          <a
+            onClick={() => setPointerNav("/")}
+            className={`mr-5 ${
+              pointernav === "/"
+                ? "text-blue-400 bg-blue-100 p-1 px-2 rounded-lg"
+                : ""
+            }`}
+          >
+            Home
+          </a>
         </Link>
         <Link href="/about">
-          <a className="mr-5 hover:text-gray-900">About</a>
+          <a
+            onClick={() => setPointerNav("/about")}
+            className={`mr-5 ${
+              pointernav === "/about"
+                ? "text-blue-400 bg-blue-100 p-1 px-2 rounded-lg"
+                : ""
+            }`}
+          >
+            About
+          </a>
         </Link>
         <Link href="/shop">
-          <a className="mr-5 hover:text-gray-900">Shop</a>
+          <a
+            onClick={() => setPointerNav("/shop")}
+            className={`mr-5 ${
+              pointernav === "/shop"
+                ? "text-blue-400 bg-blue-100 p-1 px-2 rounded-lg"
+                : ""
+            }`}
+          >
+            Shop
+          </a>
         </Link>
         <Link href="/contact">
-          <a className="hover:text-gray-900 mr-5">Contact</a>
+          <a
+            onClick={() => setPointerNav("/contact")}
+            className={`mr-5 ${
+              pointernav === "/contact"
+                ? "text-blue-400 bg-blue-100 p-1 px-2 rounded-lg"
+                : ""
+            }`}
+          >
+            Contact
+          </a>
         </Link>
         <Link href="/user">
-          <a className="hover:text-gray-900">User</a>
+          <a
+            onClick={() => setPointerNav("/user")}
+            className={`mr-5 ${
+              pointernav === "/user"
+                ? "text-blue-400 bg-blue-100 p-1 px-2 rounded-lg"
+                : ""
+            }`}
+          >
+            User
+          </a>
         </Link>
       </div>
       <div className="order-1 md:order-2 w-full md:w-1/3 flex justify-center hover:font-bold">
         <Link href="/shop">
-          <div className="w-full flex justify-center items-center ">
+          <div
+            onClick={() => setPointerNav("/shop")}
+            className="w-full flex justify-center items-center "
+          >
             <Image
               src="/shopping-cart-svgrepo-com.svg"
               alt="Picture of the author"
@@ -52,7 +101,12 @@ function Navbar({ cart }: any) {
         </Link>
       </div>
       <div className="order-3 w-full md:w-1/3 flex justify-center lg:justify-end items-center pt-3 md:pt-0 lg:ml-0">
-        <div className="mr-5">
+        <div
+          onClick={() => setPointerNav("/cart")}
+          className={`mr-5 ${
+            pointernav === "/cart" ? "bg-blue-100 p-1 rounded-full" : ""
+          }`}
+        >
           <Link href="/cart">
             <IconButton aria-label="cart">
               <StyledBadge badgeContent={cart?.length} color="secondary">
